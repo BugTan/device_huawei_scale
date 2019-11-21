cde1a0156003087cd2dc62bef020dc33925e2742
Albert I <kras@raphielgang.org>
2019-11-22 00:46:02 +0800

    scale: init: property_set -> property_override
    Proper, device-specific build fingerprint is never being set because
    they have been already set by build.prop. Make them overriding instead.
    
    Change-Id: Iab23f6bd6b06fec936b6da33f860082e6e842768
    Signed-off-by: Albert I <kras@raphielgang.org>

--- init/init_scale.cpp
+++ init/init_scale.cpp
@@ -92,98 +92,98 @@ void init_target_properties()
     /* SCL-AL00 */
     if (buf.find("SCL-AL00") != string::npos) {
         set_model("SCL-AL00");
-        property_set("ro.build.description", "SCL-AL00-user 5.1.1 GRJ90 C00B251 release-keys");
-        property_set("ro.build.fingerprint", "Honor/SCL-AL00/hnSCL-Q:5.1.1/HonorSCL-AL00/C00B251:user/release-keys");
+        property_override("ro.build.description", "SCL-AL00-user 5.1.1 GRJ90 C00B251 release-keys");
+        property_override("ro.build.fingerprint", "Honor/SCL-AL00/hnSCL-Q:5.1.1/HonorSCL-AL00/C00B251:user/release-keys");
 
     /* SCL-CL00 */
     } else if (buf.find("SCL-CL00") != string::npos) {
         set_model("SCL-CL00");
-        property_set("ro.build.description", "SCL-CL00-user 5.1.1 GRJ90 C92B261 release-keys");
-        property_set("ro.build.fingerprint", "Honor/SCL-CL00/hnSCL-Q:5.1.1/HonorSCL-CL00/C92B261:user/release-keys");
+        property_override("ro.build.description", "SCL-CL00-user 5.1.1 GRJ90 C92B261 release-keys");
+        property_override("ro.build.fingerprint", "Honor/SCL-CL00/hnSCL-Q:5.1.1/HonorSCL-CL00/C92B261:user/release-keys");
 
     /* SCL-L01 */
     } else if (buf.find("SCL-L01") != string::npos) {
         set_model("SCL-L01");
-        property_set("ro.build.description", "SCL-L01-user 5.1.1 GRJ90 C432B181 release-keys");
-        property_set("ro.build.fingerprint", "Huawei/SCL-L01/hwSCL-Q:5.1.1/HuaweiSCL-L01/C432B181:user/release-keys");
+        property_override("ro.build.description", "SCL-L01-user 5.1.1 GRJ90 C432B181 release-keys");
+        property_override("ro.build.fingerprint", "Huawei/SCL-L01/hwSCL-Q:5.1.1/HuaweiSCL-L01/C432B181:user/release-keys");
 
     /* SCL-L02 */
     } else if (buf.find("SCL-L02") != string::npos) {
         set_model("SCL-L02");
-        property_set("ro.build.description", "SCL-L02-user 5.1.1 GRJ90 C576B130 release-keys");
-        property_set("ro.build.fingerprint", "Huawei/SCL-L02/hwSCL-Q:5.1.1/HuaweiSCL-L02/C576B130:user/release-keys");
+        property_override("ro.build.description", "SCL-L02-user 5.1.1 GRJ90 C576B130 release-keys");
+        property_override("ro.build.fingerprint", "Huawei/SCL-L02/hwSCL-Q:5.1.1/HuaweiSCL-L02/C576B130:user/release-keys");
 
     /* SCL-L03 */
     } else if (buf.find("SCL-L03") != string::npos) {
         set_model("SCL-L03");
-        property_set("ro.build.description", "SCL-L03-user 5.1.1 GRJ90 C25B143 release-keys");
-        property_set("ro.build.fingerprint", "Huawei/SCL-L03/hwSCL-Q:5.1.1/HuaweiSCL-L03/C25B143:user/release-keys");
+        property_override("ro.build.description", "SCL-L03-user 5.1.1 GRJ90 C25B143 release-keys");
+        property_override("ro.build.fingerprint", "Huawei/SCL-L03/hwSCL-Q:5.1.1/HuaweiSCL-L03/C25B143:user/release-keys");
 
     /* SCL-L04 */
     } else if (buf.find("SCL-L04") != string::npos) {
         set_model("SCL-L04");
-        property_set("ro.build.description", "SCL-L04-user 5.1.1 GRJ90 C654B141 release-keys");
-        property_set("ro.build.fingerprint", "Huawei/SCL-L04/hwSCL-Q:5.1.1/HuaweiSCL-L04/C654B141:user/release-keys");
+        property_override("ro.build.description", "SCL-L04-user 5.1.1 GRJ90 C654B141 release-keys");
+        property_override("ro.build.fingerprint", "Huawei/SCL-L04/hwSCL-Q:5.1.1/HuaweiSCL-L04/C654B141:user/release-keys");
 
     /* SCL-L21 */
     } else if (buf.find("SCL-L21") != string::npos) {
         set_model("SCL-L21");
-        property_set("ro.build.description", "SCL-L21-user 5.1.1 GRJ90 C432B150 release-keys");
-        property_set("ro.build.fingerprint", "Huawei/SCL-L21/hwSCL-Q:5.1.1/HuaweiSCL-L21/C432B150:user/release-keys");
+        property_override("ro.build.description", "SCL-L21-user 5.1.1 GRJ90 C432B150 release-keys");
+        property_override("ro.build.fingerprint", "Huawei/SCL-L21/hwSCL-Q:5.1.1/HuaweiSCL-L21/C432B150:user/release-keys");
 
     /* SCL-TL00 */
     } else if (buf.find("SCL-TL00") != string::npos) {
         set_model("SCL-TL00");
-        property_set("ro.build.description", "SCL-TL00-user 5.1.1 GRJ90 C01B172 release-keys");
-        property_set("ro.build.fingerprint", "Honor/SCL-TL00/hnSCL-Q:5.1.1/HonorSCL-TL00/C01B172:user/release-keys");
+        property_override("ro.build.description", "SCL-TL00-user 5.1.1 GRJ90 C01B172 release-keys");
+        property_override("ro.build.fingerprint", "Honor/SCL-TL00/hnSCL-Q:5.1.1/HonorSCL-TL00/C01B172:user/release-keys");
 
     /* SCL-TL00H */
     } else if (buf.find("SCL-TL00H") != string::npos) {
         set_model("SCL-TL00H");
-        property_set("ro.build.description", "SCL-TL00H-user 5.1.1 GRJ90 C00B172 release-keys");
-        property_set("ro.build.fingerprint", "Honor/SCL-TL00H/hnSCL-Q:5.1.1/HonorSCL-TL00H/C00B172:user/release-keys");
+        property_override("ro.build.description", "SCL-TL00H-user 5.1.1 GRJ90 C00B172 release-keys");
+        property_override("ro.build.fingerprint", "Honor/SCL-TL00H/hnSCL-Q:5.1.1/HonorSCL-TL00H/C00B172:user/release-keys");
 
     /* SCL-TL10 */
     } else if (buf.find("SCL-TL10") != string::npos) {
         set_model("SCL-TL10");
-        property_set("ro.build.description", "SCL-TL10-user 5.1.1 GRJ90 C01B220 release-keys");
-        property_set("ro.build.fingerprint", "Honor/SCL-TL10/hnSCL-Q:5.1.1/HonorSCL-TL10/C01B220:user/release-keys");
+        property_override("ro.build.description", "SCL-TL10-user 5.1.1 GRJ90 C01B220 release-keys");
+        property_override("ro.build.fingerprint", "Honor/SCL-TL10/hnSCL-Q:5.1.1/HonorSCL-TL10/C01B220:user/release-keys");
 
     /* SCL-TL10H */
     } else if (buf.find("SCL-TL10H") != string::npos) {
         set_model("SCL-TL10H");
-        property_set("ro.build.description", "SCL-TL10H-user 5.1.1 GRJ90 C00B220 release-keys");
-        property_set("ro.build.fingerprint", "Honor/SCL-TL10H/hnSCL-Q:5.1.1/HonorSCL-TL10H/C00B220:user/release-keys");
+        property_override("ro.build.description", "SCL-TL10H-user 5.1.1 GRJ90 C00B220 release-keys");
+        property_override("ro.build.fingerprint", "Honor/SCL-TL10H/hnSCL-Q:5.1.1/HonorSCL-TL10H/C00B220:user/release-keys");
 
     /* SCL-U03 */
     } else if (buf.find("SCL-U03") != string::npos) {
         set_model("SCL-U03");
-        property_set("ro.build.description", "SCL-U03-user 5.1.1 GRJ90 C432B150 release-keys");
-        property_set("ro.build.fingerprint", "Huawei/SCL-U03/hwSCLU-Q:5.1.1/HuaweiSCL-U03/C432B150:user/release-keys");
+        property_override("ro.build.description", "SCL-U03-user 5.1.1 GRJ90 C432B150 release-keys");
+        property_override("ro.build.fingerprint", "Huawei/SCL-U03/hwSCLU-Q:5.1.1/HuaweiSCL-U03/C432B150:user/release-keys");
 
     /* SCL-U21 */
     } else if (buf.find("SCL-U21") != string::npos) {
         set_model("SCL-U21");
-        property_set("ro.build.description", "SCL-U21-user 5.1.1 GRJ90 C432B150 release-keys");
-        property_set("ro.build.fingerprint", "Huawei/SCL-U21/hwSCLU-Q:5.1.1/HuaweiSCL-U21/C432B150:user/release-keys");
+        property_override("ro.build.description", "SCL-U21-user 5.1.1 GRJ90 C432B150 release-keys");
+        property_override("ro.build.fingerprint", "Huawei/SCL-U21/hwSCLU-Q:5.1.1/HuaweiSCL-U21/C432B150:user/release-keys");
 
     /* SCL-U23 */
     } else if (buf.find("SCL-U23") != string::npos) {
         set_model("SCL-U23");
-        property_set("ro.build.description", "SCL-U23-user 5.1.1 GRJ90 C636B140 release-keys");
-        property_set("ro.build.fingerprint", "Huawei/SCL-U23/hwSCLU-Q:5.1.1/HuaweiSCL-U23/C636B140:user/release-keys");
+        property_override("ro.build.description", "SCL-U23-user 5.1.1 GRJ90 C636B140 release-keys");
+        property_override("ro.build.fingerprint", "Huawei/SCL-U23/hwSCLU-Q:5.1.1/HuaweiSCL-U23/C636B140:user/release-keys");
 
     /* SCL-U31 */
     } else if (buf.find("SCL-U31") != string::npos) {
         set_model("SCL-U31");
-        property_set("ro.build.description", "SCL-U31-user 5.1.1 GRJ90 C185B161 release-keys");
-        property_set("ro.build.fingerprint", "Huawei/SCL-U31/hwSCLU-Q:5.1.1/HuaweiSCL-U31/C185B161:user/release-keys");
+        property_override("ro.build.description", "SCL-U31-user 5.1.1 GRJ90 C185B161 release-keys");
+        property_override("ro.build.fingerprint", "Huawei/SCL-U31/hwSCLU-Q:5.1.1/HuaweiSCL-U31/C185B161:user/release-keys");
 
     /* SCC-U21 */
     } else if (buf.find("SCC-U21") != string::npos) {
         set_model("SCC-U21");
-        property_set("ro.build.description", "SCC-U21-user 5.1.1 GRJ90 C636B170 release-keys");
-        property_set("ro.build.fingerprint", "Huawei/SCC-U21/hwSCC-Q:5.1.1/HuaweiSCC-U21/C636B170:user/release-keys");
+        property_override("ro.build.description", "SCC-U21-user 5.1.1 GRJ90 C636B170 release-keys");
+        property_override("ro.build.fingerprint", "Huawei/SCC-U21/hwSCC-Q:5.1.1/HuaweiSCC-U21/C636B170:user/release-keys");
 
     } else
         LOG(ERROR) << __func__ << ": unexcepted huawei_fac_product_name!";
